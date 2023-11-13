d060b4f323711209c33c382eb2424cb4bbcb4547
Adam Stefka <275844@student.pwr.edu.pl>
2023-11-13 17:15:53 +0100

    Podstrony oraz menu

---
+++ Blackjack/Pages/Home/SymulatorGry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blackjack.Pages.Home
+{
+    public class SymulatorGryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
